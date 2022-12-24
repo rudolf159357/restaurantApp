f5f3789592b51dfdc816260528474f7a57254455
rudolf <rudigard15@gmail.com>
2022-12-24 21:22:01 +0100

    Add project files.

---
+++ restaurantApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restaurantApp.Models;
+using System.Diagnostics;
+
+namespace restaurantApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
