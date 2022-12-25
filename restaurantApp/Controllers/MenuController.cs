bbe96e61e26ff699111d78c4e10c7eb4f5b4edab
rudolf <rudigard15@gmail.com>
2022-12-25 16:35:14 +0100

    added location and food views

--- restaurantApp/Controllers/MenuController.cs
+++ restaurantApp/Controllers/MenuController.cs
@@ -4,8 +4,23 @@ namespace restaurantApp.Controllers
 {
     public class MenuController : Controller
     {
+        // menu view
         public IActionResult Index()
         {
+            return View();
+        }
+
+        // soup view
+        public IActionResult Soup() {
+            return View();
+        }
+
+        // main  meal view
+        public IActionResult Meal() {
+            return View();
+        }
+        // desserts view
+        public IActionResult Desserts() {
             return View();
         }
     }
